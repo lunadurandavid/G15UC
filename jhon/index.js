8badfe7a4981b926dc695d1c6ff24d27b30bbfd9
jhon mendez <jhonmendex@gmail.com>
2024-07-09 20:44:26 -0500

    add: new calculadora

--- jhon/index.js
+++ jhon/index.js
@@ -1,3 +1,7 @@
-console.log("version 1");
-console.log("version 1.1");
-console.log("version 2");
+import suma from "suma.js";
+
+console.log("Calculadora");
+const numero1 = 1;
+const numero2 = 2;
+
+console.log(suma(numero1, numero2));
