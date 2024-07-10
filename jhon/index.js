96436e4d9dd41f204387ada1365caf532f7b3414
jhon mendez <jhonmendex@gmail.com>
2024-07-09 21:30:24 -0500

    add: new file prueba

--- jhon/index.js
+++ jhon/index.js
@@ -9,3 +9,4 @@ console.log(suma(numero1, numero2));
 console.log(resta(numero1, numero2));
 console.log(multiplicacion(numero1, numero2));
 console.log(division(numero1, numero2));
+console.log("cambio");
