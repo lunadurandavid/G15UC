4683b07cd2f61b7b2fc8fbec7f0002390a68683c
jhon mendez <jhonmendex@gmail.com>
2024-07-09 20:50:17 -0500

    add: new file multiplicacion

--- jhon/index.js
+++ jhon/index.js
@@ -1,8 +1,10 @@
 import suma from "suma.js";
 import resta from "resta.js";
+import multiplicacion from "./multiplicacion";
 console.log("Calculadora");
 const numero1 = 1;
 const numero2 = 2;
 
 console.log(suma(numero1, numero2));
 console.log(resta(numero1, numero2));
+console.log(multiplicacion(numero1, numero2));
---
+++ jhon/multiplicacion.js
@@ -0,0 +1,3 @@
+export function multiplicacion(numero1, numero2) {
+  return numero1 * numero2;
+}
