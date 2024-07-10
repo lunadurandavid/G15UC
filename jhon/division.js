c1871562a47904a71618f9769c4f7322e59bee68
jhon mendez <jhonmendex@gmail.com>
2024-07-09 21:07:27 -0500

    add: new file division

---
+++ jhon/division.js
@@ -0,0 +1,3 @@
+export function division(numero1, numero2) {
+  return numero1 / numero2;
+}
--- jhon/index.js
+++ jhon/index.js
@@ -8,3 +8,4 @@ const numero2 = 2;
 console.log(suma(numero1, numero2));
 console.log(resta(numero1, numero2));
 console.log(multiplicacion(numero1, numero2));
+console.log(division(numero1, numero2));
